6d635469c5b28aab457810a7ae197aedb4b96aa5
Sinornn <richardvilcinsh@gmail.com>
2025-04-09 06:52:51 -0400

    RichardScene Complete

--- Assets/Enemy3D.cs
+++ Assets/Enemy3D.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 
 public class Enemy3D : MonoBehaviour
 {
-    public float speed = 2.5f; 
+    public float speed = 2.5f;
+    public AudioClip killSound;
+
     // Update is called once per frame
     void Update()
     {
         var direction  = new Vector3(112, 55, 0) - transform.position;
-        if (direction.magnitude > 100){
+        if (direction.magnitude > 95){
             transform.position += direction.normalized*Time.deltaTime*speed;
         }
     }
 
     public void Kill(){
+
         Destroy(gameObject);
+        if (killSound != null)
+        {
+            AudioSource.PlayClipAtPoint(killSound, transform.position);
+        }
     }
 }
